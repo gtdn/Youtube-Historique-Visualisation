804da962b5739a6b888b5525afa67cf84c70614a
gtdn <btdein@gmail.com>
2021-12-28 13:06:20 +0100

    color pie

--- script/camembert.js
+++ script/camembert.js
@@ -1,6 +1,6 @@
 const categ = [["29",1],["44",1],["2",2],["15",4],["19",7],["17",25],["27",64],["1",70],["26",76],["28",77],["25",106],["22",157],["23",236],["24",426],["10",799],["20",1195]]
 
-const color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
+const color = d3.scaleOrdinal(['#eca1a6','#8ca3a3','#d4ac6e','#7a3b2e','#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c', '#563f46']);
 
 var svgCam = d3.select("#codeD3Cam").append("svg")
   .attr("width", width)
