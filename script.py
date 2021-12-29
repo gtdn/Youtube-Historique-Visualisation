80d37940562813a7ed2b210e58f74427ee7751a7
LambertTimothee <info.tim.lambert@gmail.com>
2021-12-29 15:33:45 +0100

    Chargement script

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import urllib.request
 import json, requests
 import datetime
+from colorama import Fore, Back, Style
+import sys
 
 
 
@@ -14,7 +16,7 @@ def getResponse(url):
     except Exception as e:
         Logs.append(str(e)+ ", url return :"+str(operUrl.getcode())+'\n')
         error = True
-        print(e,", url return :",operUrl.getcode())
+        print(Fore.RED,"\n",e,", url return :",operUrl.getcode())
 
     return (error,jsonData)
 
@@ -32,17 +34,17 @@ apiKey = 'AIzaSyBZ-BcTFC8CFSfr4O5k_MrzpfuGw7j2H3U'
 
 #Start and End scrapping data
 debut = 1090
-fin = 1091
+fin = 1110
 
 datasRetour = []
 error = False
 for i,d in enumerate(data):
     if error==False:
         if i >= debut and i < fin:
-            if i%100 == 0:
-                print("Scrapping in progress, ",i-debut,"/",fin-debut)
+            #if i%100 == 0:
+            sys.stdout.flush()
+            print(Fore.GREEN,"\r Scrapping in progress, ",i-debut+1,"/",fin-debut,end=' ')
             try:
-                print(d)
                 video_id = d['titleUrl'][32:]
                 url = "https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id="+video_id+"&key="+apiKey
                 error, jsonData = getResponse(url)
@@ -50,18 +52,19 @@ for i,d in enumerate(data):
                 datasRetour.append(jsonData)
             except  Exception as e:
                 Logs.append('error type :'+str(e)+' during iteration :'+str(i)+'\n')
-                print('error type :',e,' during iteration :',i)
+                print(Fore.RED,'\nerror type :',e,' during iteration :',i)
     else:
         Logs.append('error during : '+str(i)+'code error : '+str(error)+'\n')
-        print('error during : ',i,'code error : ',error)
+
+        print(Fore.RED,'\nerror during : ',i,'code error : ',error)
         break;
 #Errors Logfiles
 logsFile = open('archives/logs.txt', 'a')
 current_time = datetime.datetime.now()
-logsFile.write("\n========= Scrap, Time = "+str(current_time)+" ======= \n")
+logsFile.write("\n\n========= Scrap, Time = "+str(current_time)+" ======= \n")
 logsFile.writelines(Logs)
-logsFile.write("Start at : "+str(debut)+", Stopped at :"+str(fin)+" Resulting : "+str(len(Logs))+" errors for "+str(fin - debut)+" lines. Tx error : "+str(len(Logs)/(fin-debut)))
-print("Start at : "+str(debut)+", Stopped at : "+str(fin)+" Resulting : "+str(len(Logs))+" errors for "+str(fin - debut)+" lines. Tx error : "+str(len(Logs)/(fin-debut)))
+logsFile.write("Start at: "+str(debut)+", Stopped at: "+str(fin)+". Write in file: "+str(dataFile)+". Resulting: "+str(len(Logs))+" errors for "+str(fin - debut)+" lines. Tx error: "+str(len(Logs)/(fin-debut)))
+print(Fore.WHITE,"Start at: "+str(debut)+", Stopped at: "+str(fin)+". Write in file: "+str(dataFile)+". Resulting: "+str(len(Logs))+" errors for "+str(fin - debut)+" lines. Tx error: "+str(len(Logs)/(fin-debut)))
 datasRetour = previousData + datasRetour
 datasJson = json.dumps(datasRetour, ensure_ascii=False, indent=4)
 with open(dataFile, 'w') as f:
